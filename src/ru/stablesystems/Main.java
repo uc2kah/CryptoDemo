807b5d579ab061d0d3c384d01ce76f9741aebe19
Eugene <novikov93@gmail.com>
2021-02-23 14:34:01 +0300

    SeveralCurrencies
    Added ability to use several currencies to add into wallet

--- src/ru/stablesystems/Main.java
+++ src/ru/stablesystems/Main.java
@@ -5,6 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         Fiat fiat = new Fiat();
-        fiat.createWallet(1000, Fiat.Currency.RUR);
+        fiat.createWallet(1000, Fiat.Currency.RUR, 100, Fiat.Currency.EUR);
     }
 }
--- src/ru/stablesystems/currency/Fiat.java
+++ src/ru/stablesystems/currency/Fiat.java
@@ -30,4 +30,87 @@ public class Fiat {
         }
 
     }
+    public void createWallet(int ammount, Currency currency, int ammount2, Currency currency2) {
+        double fiatCurrency;
+        switch (currency) {
+            case RUR:
+                fiatCurrency =(ammount * exchangeRateRUR);
+                break;
+            case USD:
+                fiatCurrency =ammount;
+                break;
+            case EUR:
+                fiatCurrency =(ammount * exchangeRateEUR);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + currency);
+        }
+        switch (currency2) {
+            case RUR:
+                fiatCurrency =fiatCurrency + (ammount2 * exchangeRateRUR);
+                break;
+            case USD:
+                fiatCurrency =fiatCurrency + ammount2;
+                break;
+            case EUR:
+                fiatCurrency =fiatCurrency + (ammount2 * exchangeRateEUR);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + currency);
+        }
+        try (FileWriter fileWriter = new FileWriter("C:\\Users\\novik\\Documents\\Repo\\CryptoDemo\\src\\ru\\stablesystems\\currency\\wallet.txt", false)) {
+            fileWriter.write(String.valueOf(fiatCurrency));
+        } catch (IOException exception){
+            System.out.println("Something went wrong");
+        }
+
+    }
+    public void createWallet(int ammount, Currency currency, int ammount2, Currency currency2, int ammount3, Currency currency3) {
+        double fiatCurrency;
+        switch (currency) {
+            case RUR:
+                fiatCurrency =(ammount * exchangeRateRUR);
+                break;
+            case USD:
+                fiatCurrency =ammount;
+                break;
+            case EUR:
+                fiatCurrency =(ammount * exchangeRateEUR);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + currency);
+        }
+        switch (currency2) {
+            case RUR:
+                fiatCurrency =fiatCurrency + (ammount2 * exchangeRateRUR);
+                break;
+            case USD:
+                fiatCurrency =fiatCurrency + ammount2;
+                break;
+            case EUR:
+                fiatCurrency =fiatCurrency + (ammount2 * exchangeRateEUR);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + currency);
+        }
+        switch (currency3) {
+            case RUR:
+                fiatCurrency =fiatCurrency + (ammount3 * exchangeRateRUR);
+                break;
+            case USD:
+                fiatCurrency =fiatCurrency + ammount3;
+                break;
+            case EUR:
+                fiatCurrency =fiatCurrency + (ammount3 * exchangeRateEUR);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + currency);
+        }
+        try (FileWriter fileWriter = new FileWriter("C:\\Users\\novik\\Documents\\Repo\\CryptoDemo\\src\\ru\\stablesystems\\currency\\wallet.txt", false)) {
+            fileWriter.write(String.valueOf(fiatCurrency));
+        } catch (IOException exception){
+            System.out.println("Something went wrong");
+        }
+
+    }
 }
